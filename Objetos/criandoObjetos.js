7ab9b38e3f83f5691600601107ada89ad4109fe2
JulianoHenri <49698948+JulianoHenri@users.noreply.github.com>
2020-01-31 15:38:32 -0300

    Objetos Javascript

---
+++ Objetos/criandoObjetos.js
@@ -0,0 +1,46 @@
+// Usando notação literal
+const obj1 = {}
+console.log(obj1)
+
+// Object em JS // Função Construtora
+console.log(typeof Object, typeof new Object)
+const obj2 = new Object
+console.log(obj2)
+
+// Funçoes contrutoras
+function Produto (nome, preco, desconto){
+    this.nome = nome
+    this.getPrecoComDescont = () =>{ //fica escondido dentro da função
+        return preco * (1 - desconto)
+    }   
+}
+
+const p1 = new Produto ('Caneta', 7.99, 0.15 )
+const p2 = new Produto ('Caneta', 2998.99, 0.25 )
+console.log(p1.getPrecoComDescont(), p2.getPrecoComDescont()) //Não esquecer de usar parenteses
+
+//Função Factory
+function criarFuncionario (nome, salarioBase, faltas){
+    return{
+        nome,
+        salarioBase,
+        faltas,
+        getSalario (){
+            return (salarioBase / 30) *(30 - faltas)
+        }
+    }
+}
+
+const f1 = criarFuncionario('Joao', 7980, 4)
+const f2 = criarFuncionario ('Maria', 11400, 1)
+console.log(f1.getSalario())
+console.log(f2.getSalario())
+
+//Object.create
+const filha = Object.create (null)
+filha.nome = 'Ana'
+console.log(filha)
+
+//Uma função famosa que retorna um Objeto
+const fromJSON = JSON.parse('{"info": "Sou um JSON"}')
+console.log(fromJSON.info)
